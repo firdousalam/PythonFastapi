20ee446d5c64501aee5e931d316de59752a1b165
Firdous Alam <firdous.alam2058@gmail.com>
2026-02-22 00:09:25 +0530

    Reading File and use Query to read data

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query
+from service.products import get_all_products
 
 app = FastAPI()
 
@@ -8,7 +9,31 @@ def home():
     return {"message": "Hellow World Fastapi"}
 
 
-@app.get("/products/{id}")
-def get_products(id: int):
-    products = ["laptop", "mobile", "tv", "ipad"]
-    return products[id]
+@app.get("/products/")
+def get_products():
+    return get_all_products()
+
+
+# read data by params
+# @app.get("/products/{id}")
+# def get_products(id: int):
+#     products = ["laptop", "mobile", "tv", "ipad"]
+#     return products[id] if products[id] else HTTPException(status_code=403, details="Product not found")
+
+# read data by query
+# curl -X 'GET' \
+#   'http://127.0.0.1:8000/productsByName?name=tv' \
+#   -H 'accept: application/json'
+@app.get("/productsByName")
+def get_product_by_name(name: str = Query(default=None, min_length=1, max_length=20, description="search product by name")):
+    products = get_products()
+    if name:
+        needle = name.strip().lower()
+        products = [p for p in products if needle in p.get("name", "").lower()]
+
+    if not products:
+        raise HTTPException(
+            status_code=404, detail=f"Product not found with name={name}")
+
+    product_length = len(products)
+    return {"total": product_length, "products": products}
