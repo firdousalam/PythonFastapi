30ffd5ea0c24db8be1f5b33b173472e1a52a87cf
Firdous Alam <firdous.alam2058@gmail.com>
2026-02-21 23:18:20 +0530

    sending dynamic data

--- app/main.py
+++ app/main.py
@@ -6,3 +6,9 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Hellow World Fastapi"}
+
+
+@app.get("/products/{id}")
+def get_products(id: int):
+    products = ["laptop", "mobile", "tv", "ipad"]
+    return products[id]
